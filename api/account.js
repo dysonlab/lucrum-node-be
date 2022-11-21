d37e29568b75d24f52473acd703e9a0bef6dab53
Reizkian <reizkian@vouchconcierge.com>
2022-11-21 09:19:15 +0700

    Add /account API documentation

--- api/account.js
+++ api/account.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createAccount} = require("../controller/account");
+const {
+        createAccount,
+        deleteAccount,
+        getAccount,
+        updateAccount
+    } = require("../controller/account");
 
 /**
  * @swagger
@@ -20,7 +25,7 @@ const {createAccount} = require("../controller/account");
  *           type: string
  *           description: account name
  *         amount:
- *           type: integer
+ *           type: number
  *           description: amount of money in an account
  *         description:
  *           type: string
@@ -73,5 +78,111 @@ const {createAccount} = require("../controller/account");
  *               message: string
  */
 router.post("/account", createAccount);
+
+/**
+ * @swagger
+ * /account/{accountId}:
+ *   delete:
+ *     summary: Delete an account
+ *     parameters:
+ *       - in: path
+ *         name: accountId
+ *         schema:
+ *           type: string
+ *           required: true
+ *     tags: [Account]
+ *     responses:
+ *       200:
+ *         description: success delete account
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ *       500:
+ *         description: internal server error
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ */
+router.delete("/account/:accountId", deleteAccount)
+
+/**
+ * @swagger
+ * /account/{userId}:
+ *   get:
+ *     summary: Get all account of a user 
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         schema:
+ *           type: string
+ *           required: true
+ *     tags: [Account]
+ *     responses:
+ *       200:
+ *         description: success get account
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ *       500:
+ *         description: internal server error
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ */
+router.get("/account/:userId", getAccount);
+
+/**
+ * @swagger
+ * /account/{accountId}:
+ *   put:
+ *     summary: Update an account
+ *     parameters:
+ *       - in: path
+ *         name: accountId
+ *         schema:
+ *           type: string
+ *           required: true
+ *     tags: [Account]
+ *     requestBody:
+ *       description: Payload data to update account group
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               accountGroup:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               amount:
+ *                 type: number
+ *               description:
+ *                 type: string 
+ *             example:
+ *                 accountGroup: 637ad206054b18c94639cca3
+ *                 name: BCA
+ *                 amount: 42000000
+ *                 description: This is Bank Central Asia account
+ *                
+ *     responses:
+ *       200:
+ *         description: success update account
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ *       500:
+ *         description: internal server error
+ *         contents:
+ *            application/json:
+ *              schema:
+ *                message: string
+ */
+ router.put("/account/:accountId", updateAccount);
 
 module.exports = router;
