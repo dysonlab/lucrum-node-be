2d664189336ee5f7229e846f115cdba2c9b792ca
Reizkian <reizkian@vouchconcierge.com>
2022-11-23 08:45:28 +0700

    Finished /expense-subcategory API

--- controller/expenseSubcategory.js
+++ controller/expenseSubcategory.js
@@ -24,3 +24,46 @@ exports.createExpenseSubcategory = (req, res) => {
     });
   };
   
+  exports.deleteExpenseSubcategory = async (req, res) => {
+    // deconstruct request params
+    const { expenseSubcategoryId } = req.params;
+  
+    try{
+      await expense_subcategory.deleteOne({ _id: expenseSubcategoryId}).exec();
+      return res.status(200).json({message: `expense subcategory ${expenseSubcategoryId} is deleted`})
+    }catch(error){
+      console.log(error)
+      return res.status(500).json({ message: "internal server error" });
+    }
+  }
+
+  exports.getExpenseSubcategory = async (req, res) => {
+    // deconstruct request params
+    const { userId } = req.params;
+  
+    try{
+      const result = await expense_subcategory.find({user:userId}).exec()
+      return res.status(200).json({ message: `success get expense category`, expenseSubcategory: result });
+    }catch(error){
+      console.log(error)
+      return res.status(500).json({ message: `cannot find expense category from user ${userId}` });
+    }
+  }
+
+  exports.updateExpenseSubcategory = async (req, res) => {
+    // deconstruct request params
+    const { expenseSubcategoryId } = req.params;
+    // deconstruct request body
+    const { expenseCategory, name } = req.body;
+    
+    try{
+      const result = await expense_subcategory.findByIdAndUpdate(expenseSubcategoryId, {
+        expenseCategory: expenseCategory,
+        name: name
+      }, {new: true}).exec()
+      return res.status(200).json({ message: `success update expense category ${result._id}`, expenseSubcategory: result });
+    }catch(error){
+      console.log(error)
+      return res.status(500).json({ message: `error updating expense category ${expenseSubcategoryId}` });
+    }
+  }
