a68655f1cc6ce051a8948394b9ee8234ba993191
Reizkian <reizkian@vouchconcierge.com>
2022-12-24 22:34:02 +0700

    Add token services

--- controller/auth.js
+++ controller/auth.js
@@ -1,8 +1,10 @@
-const {user} = require("../model");
+const { user } = require("../model");
+const { signToken } = require("../service/token");
 
 exports.login = (req, res) => {
   // deconstruct request body
   const {email, password} = req.body;
+
   // find user with email, then update lastLogon
   user.findOneAndUpdate({email: email}, {lastLogon: Date.now()}, {new: true}, (error, user) => {
     if (error) {
@@ -15,7 +17,13 @@ exports.login = (req, res) => {
         }
         if (isMatch) {
           console.log(`login success ${user._id}`);
-          return res.status(200).json({message: `login success ${user._id}`});
+          const token = signToken({
+            user: user._id,
+            email: user.email,
+            name: user.name,
+            lastLogon: user.lastLogon,
+          })
+          return res.status(200).json({message: `login success ${user._id}`, token: token});
         } else {
           console.log(`login failed ${user._id}`);
           return res.status(401).json({message: "unauthorized access"});
