a204ef3a20c6d62e1aae75af676a1aec2a8f721a
Reizkian <reizkian@vouchconcierge.com>
2023-01-14 23:42:26 +0700

    Handle user null

--- controller/auth.js
+++ controller/auth.js
@@ -3,32 +3,39 @@ const { signToken } = require("../service/token");
 
 exports.login = (req, res) => {
   // deconstruct request body
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   // find user with email, then update lastLogon
-  user.findOneAndUpdate({email: email}, {lastLogon: Date.now()}, {new: true}, (error, user) => {
+  user.findOneAndUpdate({ email: email }, { lastLogon: Date.now() }, { new: true }, (error, user) => {
     if (error) {
       console.log(error);
-      return res.status(500).json({message: "internal server error"});
-    } else {
-      user.comparePassword(password, function(error, isMatch) {
-        if (error) {
-          console.log(error);
-        }
-        if (isMatch) {
-          console.log(`login success ${user._id}`);
-          const token = signToken({
-            user: user._id,
-            email: user.email,
-            name: user.name,
-            lastLogon: user.lastLogon,
-          })
-          return res.status(200).json({message: `login success ${user._id}`, token: token});
-        } else {
-          console.log(`login failed ${user._id}`);
-          return res.status(401).json({message: "unauthorized access"});
-        }
-      });
+      return res.status(500).json({ message: "internal server error" });
     }
+
+    if(user === null){
+      return res.status(404).json({ message: `user with email ${email} is not found`});
+    }
+
+    user.comparePassword(password, function (error, isMatch) {
+      if (error) {
+        console.log(error);
+        res.status(404).json({ message: "user not found" });
+      }
+      if (isMatch) {
+        console.log(`login success ${user._id}`);
+        const token = signToken({
+          user: user._id,
+          email: user.email,
+          name: user.name,
+          lastLogon: user.lastLogon,
+        })
+        return res.status(200).json({ message: `login success ${user._id}`, token: token });
+      } 
+      if(!isMatch){
+        console.log(`login failed ${user._id}`);
+        return res.status(401).json({ message: "unauthorized access" });
+      }
+    });
+
   });
 };
